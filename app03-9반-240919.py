ef8feebe27493c0fb15c2deabe08ac2118399a81
Mr-Jamsil <163809782+Mr-Jamsil@users.noreply.github.com>
2024-09-23 09:18:44 +0900

    Update app03-9반-240919.py

--- app03-9반-240919.py
+++ app03-9반-240919.py
@@ -1,7 +1,8 @@
-import pygame
 from pathlib import Path
+
+import pygame
 from pygame import Rect
-from pygame.sprite import Sprite, LayeredUpdates
+from pygame.sprite import LayeredUpdates, Sprite
 from pygame.surface import Surface
 from pygame.time import Clock
 
@@ -17,48 +18,45 @@ ICON_DIR = PROJ_DIR / "assets" / "icons"
 
 pygame.init()
 
-pygame.display.set_caption("Flappy Bird")
 pygame.display.set_caption(TITLE)
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def get_sprite(name):
     return pygame.image.load(SPRITE_DIR / f"{name}.png")
-
-pygame.image.load("이미지파일명.png")
-
-sprites = LayeredUpdates()
-clock = Clock()
 
 
 class Bird(Sprite):
     GRAVITY = 0.4
     FLAP_STRENGTH = 6
-    
+
     def __init__(self, *groups):
         self.images = [
             get_sprite("redbird-0"),
             get_sprite("redbird-1"),
-            get_sprite("redbird-2")
+            get_sprite("redbird-2"),
         ]
         self.image = get_sprite("redbird-0")
         self.rect = self.image.get_rect(topleft=(50, 50))
         self.fall_speed = 0
         super().__init__(*groups)
-        
+
     def update(self):
         self.images.insert(0, self.images.pop())
         self.image = self.images[0]
-        
+
         GRAVITY = 0.4
         self.fall_speed += GRAVITY
         self.rect.y += self.fall_speed
-        
+
     def handle_event(self, event):
         if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
             self.fall_speed = -6
-            
-            
+
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+sprites = LayeredUpdates()
+clock = Clock()
+
 bird = Bird(sprites)
 
 running = True
@@ -66,12 +64,11 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        
+
         bird.handle_event(event)
 
-    screen.fill(0)  # (0,0,0) RGB = black
-    screen.fill("pink")
-    
+    screen.fill(0)  # 0 = (0,0,0) = black
+
     sprites.draw(screen)
     sprites.update()
 
